aed0deffe400849ebcc5e91982cc2a05f1d7abf1
okihaito <61840659+okihaito@users.noreply.github.com>
2021-02-07 15:46:53 +0100

    + Funzioni

--- Tempo Libero/programminorandom.cpp
+++ Tempo Libero/programminorandom.cpp
@@ -1,13 +1,21 @@
 /*-------------------------------------------------------------------------------------------------
                         Programmino Random...
                                         Giusto per perdere un po' di tempo.
-                                                                        Aggiornato il 29/01/2021.
+                                                                        Aggiornato il 07/02/2021.
 -------------------------------------------------------------------------------------------------*/
 
 #include <iostream>
 #include <string>
 #include <cmath>
 using namespace std;
+
+//FUNZIONI
+float Somma(float a, float b);
+float Differenza(float a, float b);
+float Moltiplicazione(float a, float b);
+float Divisione(float a, float b);
+float Radice(float a);
+float Esponente(float base, float esp);
 
 int main()
 {
@@ -17,7 +25,6 @@ int main()
     //calc
    float a,b,e,esp,base;
    int t;
-   long double risultato;
 
    //INIZIO
    a=0;
@@ -33,13 +40,14 @@ int main()
    switch(t)
    {
    case 1: //somma
-     cout << "Inserisci due numeri (SOMMA):" << endl;
-    cin >> a >> b;
-    risultato=a+b;
-    cout << "La somma e: " << risultato << endl;
-    cout << "+-----------+" << endl << "| Seleziona |" << endl <<  "+-----------+" << endl <<  "| 1 = ESCI  |" << endl <<  "+-----------+" << endl;
-    cin >> e;
-    if(e==1){ //esci
+       float sysf; //PER LA FUNZIONE
+       cout << "Inserisci due numeri (SOMMA):" << endl;
+       cin >> a >> b;
+        sysf= Somma(a,b); //CHIAMA LA FUNZIONE SOMMA
+        cout << "Il risultato e': " << sysf << endl;
+        cout << "+-----------+" << endl << "| Seleziona |" << endl <<  "+-----------+" << endl <<  "| 1 = ESCI  |" << endl <<  "+-----------+" << endl; //MENU
+        cin >> e;
+        if(e==1){ //esci
         while(i<sys){
         system("CLS"); //pulisci 5 volte
         i++;
@@ -50,9 +58,9 @@ int main()
    case 2: //differenza
     cout << "Inserisci due numeri (DIFFERENZA):" << endl;
     cin >> a >> b;
-    risultato=a-b;
-    cout << "La differenza e: " << risultato << endl;
-    cout << "+-----------+" << endl << "| Seleziona |" << endl <<  "+-----------+" << endl <<  "| 1 = ESCI  |" << endl <<  "+-----------+" << endl;
+    sysf= Differenza(a,b); //CHIAMA LA FUNZIONE DIFFERENZA
+    cout << "La differenza e: " << sysf << endl;
+    cout << "+-----------+" << endl << "| Seleziona |" << endl <<  "+-----------+" << endl <<  "| 1 = ESCI  |" << endl <<  "+-----------+" << endl; //MENU
     cin >> e;
     if(e==1){ //esci
         while(i<sys){
@@ -65,9 +73,9 @@ int main()
    case 3: //moltiplicazione
     cout << "Inserisci due numeri (MOLTIPLICAZIONE):" << endl;
     cin >> a >> b;
-    risultato=a*b;
-    cout << "La moltiplicazione e: " << risultato << endl;
-    cout << "+-----------+" << endl << "| Seleziona |" << endl <<  "+-----------+" << endl <<  "| 1 = ESCI  |" << endl <<  "+-----------+" << endl;
+    sysf= Moltiplicazione(a,b); //CHIAMA LA FUNZIONE MOLTIPLICAZIONE
+    cout << "La moltiplicazione e: " << sysf << endl;
+    cout << "+-----------+" << endl << "| Seleziona |" << endl <<  "+-----------+" << endl <<  "| 1 = ESCI  |" << endl <<  "+-----------+" << endl; //MENU
     cin >> e;
     if(e==1){ //esci
         while(i<sys){
@@ -80,9 +88,9 @@ int main()
    case 4: //divisione
     cout << "Inserisci due numeri (DIVISIONE):" << endl;
     cin >> a >> b;
-    risultato=a/b;
-    cout << "La divisione e: " << risultato << endl;
-    cout << "+-----------+" << endl << "| Seleziona |" << endl <<  "+-----------+" << endl <<  "| 1 = ESCI  |" << endl <<  "+-----------+" << endl;
+    sysf = Divisione(a,b); //CHIAMA LA FUNZIONE DIVISIONE 
+    cout << "La divisione e: " << sysf << endl;
+    cout << "+-----------+" << endl << "| Seleziona |" << endl <<  "+-----------+" << endl <<  "| 1 = ESCI  |" << endl <<  "+-----------+" << endl; //MENU
     cin >> e;
     if(e==1){ //esci
         while(i<sys){
@@ -95,9 +103,9 @@ int main()
    case 5: //radice quadrata
     cout << "Inserisci numero (RADICE QUADRATA):" << endl;
     cin >> a;
-    risultato=sqrt(a);
-    cout << "La radice quadrata di " << a << ":" << risultato << endl;
-    cout << "+-----------+" << endl << "| Seleziona |" << endl <<  "+-----------+" << endl <<  "| 1 = ESCI  |" << endl <<  "+-----------+" << endl;
+    sysf= Radice(a); //CHIAMA LA FUNZIONE RADICE
+    cout << "La radice quadrata di " << a << " : " << sysf << endl;
+    cout << "+-----------+" << endl << "| Seleziona |" << endl <<  "+-----------+" << endl <<  "| 1 = ESCI  |" << endl <<  "+-----------+" << endl; //MENU
     cin >> e;
      if(e==1){ //esci
         while(i<sys){
@@ -112,9 +120,9 @@ int main()
     cin >> base;
     cout << "Inserisci esponente:" << endl;
     cin >> esp;
-    risultato=pow(base,esp);
-    cout << "Il risultato e': " << risultato << endl;
-    cout << "+-----------+" << endl << "| Seleziona |" << endl <<  "+-----------+" << endl <<  "| 1 = ESCI  |" << endl <<  "+-----------+" << endl;
+    sysf= Esponente(base,esp); //CHIAMA LA FUNZIONE ESPONENTE
+    cout << "Il risultato e': " << sysf << endl;
+    cout << "+-----------+" << endl << "| Seleziona |" << endl <<  "+-----------+" << endl <<  "| 1 = ESCI  |" << endl <<  "+-----------+" << endl; //MENU
     cin >> e;
     if(e==1){ //esci
         while(i<sys){
@@ -123,7 +131,46 @@ int main()
         }
         return main();
     }
-   //case 7:
+    return 0; 
+    }}
 
-    return 0;
-}}
+float Somma(float a,float b) //FUNZIONE SOMMA
+{
+    float s; //NUOVA VARIABILE
+    s=a+b;
+    return s; //RITORNA INDIETRO CON IL RISULTATO
+}
+float Differenza(float a,float b) //FUNZIONE SOTTRAZIONE 
+{
+    float s; //NUOVA VARIABILE
+    s=a-b;
+    return s; //RITORNA INDIETRO CON IL RISULTATO
+}
+float Moltiplicazione(float a,float b) //FUNZIONE MOLTIPLICAZIONE
+{
+    float s; //NUOVA VARIABILE
+    s=a*b;
+    return s; //RITORNA INDIETRO CON IL RISULTATO
+}
+float Divisione(float a,float b) //FUNZIONE DIVISIONE
+{
+    float s; //NUOVA VARIABILE
+    s=a/b;
+    return s; //RITORNA INDIETRO CON IL RISULTATO
+}
+float Radice(float a) //FUNZIONE RADICE QUADRATA
+{
+    float s; //NUOVA VARIABILE
+    s=sqrt(a);
+    if(a<0){ //SE IL NUMERO INSERITO E' NEGATIVO ALLORA E' IMPOSSIBILE
+        cout << "Errore: Il numero e' negativo!" << endl;
+        return 0; //RISULTATO 0
+    }
+    return s; //RITORNA INDIETRO CON IL RISULTATO
+}
+float Esponente(float base, float esp) //FUNZIONE ESPONENTE
+{
+    float s; //NUOVA VARIABILE
+    s=pow(base,esp);
+    return s; //RITORNA INDIETRO CON IL RISULTATO
+}
